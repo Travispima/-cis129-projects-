b08f02f991e047d5781ded5ddbfb2f042a526eec
Travispima <158644366+Travispima@users.noreply.github.com>
2024-02-08 07:18:14 -0700

    It looked wrong so I doubled the apostrophe in

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -7,7 +7,7 @@ Print('I ignored it, lost in my show. A minute later I heard a sharp, hollow thu
 print('I snapped my head in the direction of the bathroom.')
 print('Another thud sounded, not as loud as the first but equally scary.')
 print('I shot to my feet, eyes fixed on the open bathroon door.')
-print('I shouted, "who's there!". A startled yelp, followed by a crash could be heard.')
+print('I shouted, "who''s there!". A startled yelp, followed by a crash could be heard.')
 print('I rushed to the door and flicked on the lights.')
 print('A puffy, black and white cat glared at me from the tub.')
 print('I noted that my shampoo and soap had been knocked down.')
