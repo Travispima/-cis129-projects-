c32b611680a04a436df6dee81d51f9023a3991fd
Travispima <158644366+Travispima@users.noreply.github.com>
2024-02-08 07:16:39 -0700

    I added a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,14 @@
-print('Hello, World!')
+# Author: Travis Greenberg
+# My first Python lab in my CIS129 course
+# A short story
+print('I was sitting on the couch one night when I heard a curious noise.')
+print('It sounded as if someone was slowly dragging a full grocery bag across the carpet.')
+Print('I ignored it, lost in my show. A minute later I heard a sharp, hollow thud.')
+print('I snapped my head in the direction of the bathroom.')
+print('Another thud sounded, not as loud as the first but equally scary.')
+print('I shot to my feet, eyes fixed on the open bathroon door.')
+print('I shouted, "who's there!". A startled yelp, followed by a crash could be heard.')
+print('I rushed to the door and flicked on the lights.')
+print('A puffy, black and white cat glared at me from the tub.')
+print('I noted that my shampoo and soap had been knocked down.')
+print('I ran.')
